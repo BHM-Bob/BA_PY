9d462e05f7de45659704b6a14343e375f3c40740
BHM-Bob <2262029386@qq.com>
2023-04-22 21:53:40 +0800

    add pca in stats sub-module

--- mbapy/stats/__init__.py
+++ mbapy/stats/__init__.py
@@ -2,10 +2,10 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2022-12-09 17:24:18
 LastEditors: BHM-Bob
-LastEditTime: 2023-04-06 20:50:50
+LastEditTime: 2023-04-19 19:56:16
 Description: 
 '''
-from . import geography, reg, test
+from . import geography, reg, test, df
 
 # def main():
 #     pass
@@ -18,3 +18,11 @@ func : func_name
 global var: globalVarName
 class : ClassName
 """
+
+import pandas as pd
+from sklearn.decomposition import PCA
+
+def pca(df:pd.DataFrame, out_dim:int):
+    pca = PCA(n_components=out_dim)
+    pca.fit(df)
+    return pca.transform(df)
