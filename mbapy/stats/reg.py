2acfd2ec1bceb5bb276f8b0445ad6978ece3cd3c
BHM-Bob G <2262029386@qq.com>
2023-08-07 19:15:07 +0800

    add stats.reg.KMeans add norm support for stats.reg.cluster

--- mbapy/stats/reg.py
+++ mbapy/stats/reg.py
@@ -1,14 +1,14 @@
 '''
 Author: BHM-Bob 2262029386@qq.com
 Date: 2023-04-06 20:44:44
-LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2023-08-02 23:35:32
+LastEditors: BHM-Bob G 2262029386@qq.com
+LastEditTime: 2023-08-07 19:15:07
 Description: 
 '''
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import (DBSCAN, Birch, KMeans, MeanShift, MiniBatchKMeans,
+from sklearn.cluster import (DBSCAN, Birch, KMeans as sk_KMeans, MeanShift, MiniBatchKMeans,
                              AgglomerativeClustering, AffinityPropagation)
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LinearRegression
@@ -16,10 +16,10 @@ from sklearn.mixture import GaussianMixture
 
 if __name__ == '__main__':
     # dev mode
-    from mbapy.base import put_err, set_default_kwargs
+    from mbapy.base import put_err, set_default_kwargs, autoparse, get_default_for_None
 else:
     # release mode
-    from ..base import put_err, set_default_kwargs
+    from ..base import put_err, set_default_kwargs, autoparse, get_default_for_None
 
 def linear_reg(x:str, y:str, df:pd.DataFrame):
     """
@@ -50,11 +50,90 @@ def linear_reg(x:str, y:str, df:pd.DataFrame):
         'r2':equation_r2,
     }
     
+class KMeans:
+    """
+    Parameters:
+        - n_clusters(int): number of clusters, if set to None, will auto search from 1 to sum of data to make fit for tolerance.
+    """
+    @autoparse
+    def __init__(self, n_clusters:int = None, tolerance:float = 0.0001, max_iter:int = 1000,
+                 init_method = 'prob', **kwargs) -> None:
+        self.centers = None # should be a ndarray with shape [n, D]
+        self.data_group_id = None
+        
+    def reset(self, **kwargs):
+        self.centers = None
+        self.data_group_id = None
+        
+    def _calcu_length_mat(self, data:np.ndarray, centers:np.ndarray = None,
+                      backend:str = 'numpy'):
+        centers = get_default_for_None(centers, self.centers)
+        if backend == 'numpy':
+            # TODO : 多算(D-1)/D * 100%
+            sub = np.subtract.outer(data, centers)[:, :, :, 0].transpose(0, 2, 1) # [N, n, D]
+            return np.linalg.norm(sub, axis = -1) # [N, n]
+        else:
+            raise NotImplementedError
+        
+    def _choose_center_from_data(self, data: np.ndarray, centers:np.ndarray):
+        # get length for every data to every centers, and calcu sum for each data
+        length = self._calcu_length_mat(data, centers).sum(axis = -1) # [N, ]
+        # get new_center_idx by prob or just using max
+        if self.init_method == 'prob':                
+            prob = length / np.sum(length)
+            idx = np.random.choice(data.shape[0], p=prob)
+        elif self.init_method == 'max':
+            idx = np.argmax(length)
+        return data[idx]
+        
+    def _init_centers(self, data:np.ndarray):
+        # choose the first center randomly
+        first_center_idx = int(np.random.uniform(0, data.shape[0]))
+        self.centers = data[first_center_idx].reshape(1, -1) # [n=1, D]
+        # generate left centers by kmeans++
+        for _ in range(self.n_clusters - 1):
+            new_center = self._choose_center_from_data(data, self.centers)
+            self.centers = np.vstack([self.centers, new_center])
+        return self.centers
+    
+    def fit(self, data:np.ndarray, **kwargs):
+        self._init_centers(data)
+        for _ in range(self.max_iter):
+            # sort data to groups id by now centers, get data_group_id
+            new_centers = np.zeros([self.n_clusters, data.shape[-1]])
+            length_mat = self._calcu_length_mat(data) # [N, n]
+            data_group_id = length_mat.argmin(axis = -1) # [N, ]
+            # sort data to groups, set new centers to be the mean of each group
+            print(np.unique(data_group_id))
+            for group_x_id in range(self.n_clusters):
+                group_x_data_id = np.argwhere(data_group_id == group_x_id).reshape(-1) # data id for one group
+                if group_x_data_id.shape[0] == 0:
+                    # empty group, perform init for this center
+                    non_null_centers = self.centers[np.unique(data_group_id)]
+                    new_centers[group_x_id] = self._choose_center_from_data(data, non_null_centers)
+                else:
+                    new_centers[group_x_id] = data[group_x_data_id].mean(axis=0)
+            # check if diff of old and new centers fit to tolerance
+            if np.sum(np.abs(new_centers - self.centers) / self.centers) * 100.0 < self.tolerance:
+                break
+            # move centers to new centers(average of groups)
+            self.centers = new_centers
+            self.data_group_id = data_group_id
+        return self.centers
+
+    def predict(self, data:np.ndarray):
+        length_mat = self._calcu_length_mat(data) # [N, n]
+        return length_mat.argmin(axis = -1) # [N, ]
+        
+    def fit_predict(self, data:np.ndarray, predict_data = None, **kwargs):
+        self.fit(data)
+        return self.predict(get_default_for_None(predict_data, data))
+    
 cluster_support_methods = ['DBSCAN', 'Birch', 'KMeans', 'MiniBatchKMeans',
                            'MeanShift', 'GaussianMixture', 'AgglomerativeClustering',
                            'AffinityPropagation']
     
-def cluster(data, n_clusters:int, method:str, **kwargs):
+def cluster(data, n_clusters:int, method:str, norm = None, norm_dim = None, **kwargs):
     """
     Notes:
         - Kmeans: 此算法尝试最小化群集内数据点的方差。K 均值最适合用于较小的数据集，因为它遍历所有数据点。
@@ -63,6 +142,13 @@ def cluster(data, n_clusters:int, method:str, **kwargs):
                 在这个混合模型中，有几个单一的高斯模型充当隐藏层。因此，
                 该模型计算数据点属于特定高斯分布的概率，即它将属于的聚类。
     """
+    # TODO : imp norm_dim
+    if norm_dim is not None:
+        raise NotImplementedError
+    if norm is not None:
+        if norm == 'div_max':
+            data = data/data.max()
+            
     if method == 'DBSCAN':
         kwargs = set_default_kwargs(kwargs, eps = 0.5, min_samples = 3)
         return DBSCAN(**kwargs).fit_predict(data)
@@ -70,9 +156,9 @@ def cluster(data, n_clusters:int, method:str, **kwargs):
         model = Birch(n_clusters=n_clusters, **kwargs)
         return model.fit_predict(data)
     elif method == 'KMeans':
-        return KMeans(n_clusters=n_clusters).fit_predict(data)
+        return sk_KMeans(n_clusters=n_clusters, n_init = 'auto').fit_predict(data)
     elif method == 'MiniBatchKMeans':
-        return MiniBatchKMeans(n_clusters=n_clusters).fit_predict(data)
+        return MiniBatchKMeans(n_clusters=n_clusters, n_init= 'auto').fit_predict(data)
     elif method == 'MeanShift':
         return MeanShift().fit_predict(data)
     elif method == 'GaussianMixture':
@@ -108,11 +194,15 @@ if __name__ == '__main__':
     pos = pca(X, 2)
     print(X.shape, pos.shape)
 
-    fig, axs = plt.subplots(2, 4, figsize = (12, 7))
-    for i in range(2):
-        for j in range(4):
-            method = cluster_support_methods[i*4+j]
-            yhat = cluster(X, n_classes, method)
+    fig, axs = plt.subplots(3, 3, figsize = (10, 10))
+    for i in range(3):
+        for j in range(3):
+            if i == 0 and j == 0:
+                method = 'mbapy-KMeans'
+                yhat = KMeans(n_classes).fit_predict(X)
+            else:
+                method = cluster_support_methods[i*3+j - 1]
+                yhat = cluster(X, n_classes, method, 'div_max')
             # 检索唯一群集
             clusters_id = np.unique(yhat)
             # 为每个群集的样本创建散点图
