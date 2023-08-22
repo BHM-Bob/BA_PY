c2ff06b170b09555f767d8934402b45fd87b5f59
BHM-Bob <2262029386@qq.com>
2023-08-22 11:41:48 +0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2022-11-01 18:30:01
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2023-07-17 00:38:34
+LastEditTime: 
 Description: 
 '''
 """
@@ -78,4 +78,4 @@ setup(
 # pip install .
 
 # python setup.py sdist
-# twine upload dist/mbapy-0.1.5.tar.gz
+# twine upload dist/mbapy-0.2.0.tar.gz
