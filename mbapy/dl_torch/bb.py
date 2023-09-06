ed7b4ba19e442803e2a895b84c1b9af1a690be7f
BHM-Bob <2262029386@qq.com>
2023-09-06 09:53:16 +0800

    add web_utils.request.random_sleep add dl_torch.bb.RoPE

--- mbapy/dl_torch/bb.py
+++ mbapy/dl_torch/bb.py
@@ -2,12 +2,12 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2023-03-23 21:50:21
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2023-05-31 10:32:32
+LastEditTime: 2023-09-06 09:51:43
 Description: Basic Blocks
 '''
 
 import math
-from typing import Union
+from typing import Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -150,6 +150,37 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         b, l, c = x.shape
         return self.pe.repeat(x.shape[0], 1, 1)[:,:l, :].add(x)
+    
+class RoPE(nn.Module):
+    """
+    from Meta LLAMA
+    edit from https://mp.weixin.qq.com/s/LH1leSGJSloxQXPYM1wzog
+    """
+    def __init__(self, dim: int, seq_len: int, theta: float = 10000.0) -> None:
+        super().__init__()
+        # 计算词向量元素两两分组之后，每组元素对应的旋转角度\theta_i
+        freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+        # 生成 token 序列索引 t = [0, 1,..., seq_len-1]
+        t = torch.arange(seq_len, device=freqs.device)
+        # freqs.shape = [seq_len, dim // 2] 
+        freqs = torch.outer(t, freqs).float()  # 计算m * \theta
+        # 计算结果是个复数向量
+        # 假设 freqs = [x, y]
+        # 则 freqs_cis = [cos(x) + sin(x)i, cos(y) + sin(y)i]
+        self.freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+    def forward(self, xq:torch.Tensor, xk:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        # xq.shape = [batch_size, seq_len, dim]
+        # xq_.shape = [batch_size, seq_len, dim // 2, 2]
+        xq_ = xq.float().reshape(*xq.shape[:-1], -1, 2)
+        xk_ = xk.float().reshape(*xk.shape[:-1], -1, 2)
+        # 转为复数域
+        xq_ = torch.view_as_complex(xq_)
+        xk_ = torch.view_as_complex(xk_)
+        # 应用旋转操作，然后将结果转回实数域
+        # xq_out.shape = [batch_size, seq_len, dim]
+        xq_out = torch.view_as_real(xq_ * self.freqs_cis).flatten(2)
+        xk_out = torch.view_as_real(xk_ * self.freqs_cis).flatten(2)
+        return xq_out.type_as(xq), xk_out.type_as(xk)
 
 class PositionwiseFeedforwardLayer(nn.Module):
     def __init__(self, hid_dim, pf_dim, dropout):
