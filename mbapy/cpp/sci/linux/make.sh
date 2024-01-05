97bf8fa76783e4170dcb1f28016c7d7af7ce8107
BHM-Bob <2262029386@qq.com>
2024-01-05 17:11:07 +0800

    fix-code: make abs path to relative path in cpp/sci/linux/make.sh

--- mbapy/cpp/sci/linux/make.sh
+++ mbapy/cpp/sci/linux/make.sh
@@ -1,9 +1,29 @@
-cd /media/bhm-bob/BHM/My_Progs/BA/Python/BA_PY/mbapy/cpp/sci/linux/
+###
+ # @Date: 2023-11-20 16:03:31
+ # @LastEditors: BHM-Bob 2262029386@qq.com
+ # @LastEditTime: 2024-01-05 17:09:54
+ # @Description: 
+### 
+
+# set wdir
+WDIR=$(cd $(dirname $0); pwd)
+cd "$WDIR"
+echo "wdir: $(pwd)"
+
+# set storage dir
+STORAGE=$(cd $(dirname $0); cd ../../storage; pwd)
+echo "storage dir: $(STORAGE)"
+
+# set build dir
 rm -r build
 echo remove build
+
+# cmake, build to build dir
 cmake -S . -B build
 cd build
 make
 echo built to build dir
-mv /media/bhm-bob/BHM/My_Progs/BA/Python/BA_PY/mbapy/cpp/sci/linux/build/libsci.so /media/bhm-bob/BHM/My_Progs/BA/Python/BA_PY/mbapy/storage/libsci.so
-echo moved libsci.so to storage dir
+
+# copy built file
+mv $WDIR/build/libsci.so $STORAGE/libsci.so
+echo libsci.so moved to storage dir
