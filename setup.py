361681621e4b4d5e138d846e8833526cb60e6574
BHM-Bob <2262029386@qq.com>
2024-03-12 22:52:00 +0800

    v0.7.1b1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2022-11-01 18:30:01
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-02-20 17:12:04
+LastEditTime: 2024-03-12 22:51:54
 Description: 
 '''
 """
@@ -108,4 +108,4 @@ setup(
 # pip install .
 
 # python setup.py sdist
-# twine upload dist/mbapy-0.7.0.tar.gz
+# twine upload dist/mbapy-0.7.1b1.tar.gz
