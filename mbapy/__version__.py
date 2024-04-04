19099e750495fb04de894b088b0f7032bf285a8d
BHM-Bob <2262029386@qq.com>
2024-04-04 19:07:58 +0800

    v0.7.1

--- mbapy/__version__.py
+++ mbapy/__version__.py
@@ -2,15 +2,15 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2022-11-01 22:16:49
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-03-12 22:49:13
+LastEditTime: 2024-04-04 19:05:03
 Description: 
 '''
 
 __title__ = "mbapy"
 __description__ = "MyBA in Python"
 __url__ = "https://github.com/BHM-Bob/BA_PY"
-__version__ = "0.7.1b1"
-__build__ = "20240312_0"
+__version__ = "0.7.1"
+__build__ = "20240404_0"
 __author__ = "BHM-Bob G"
 __author_email__ = "bhmfly@foxmail.com"
 __license__ = "MIT"
