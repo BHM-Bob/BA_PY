94b2af2fd1032130d98183c8f50340aa3e5af31a
BHM-Bob <2262029386@qq.com>
2024-05-02 16:29:24 +0800

    fix-code: scripts/extract-dir: skip when dist is source, add option to remove empty dir

--- mbapy/scripts/extract-dir.py
+++ mbapy/scripts/extract-dir.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-02-05 15:12:32
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-02-20 10:12:23
+LastEditTime: 2024-05-02 16:20:29
 Description: 
 '''
 
@@ -35,30 +35,39 @@ def main(sys_args: List[str] = None):
                             help='FLAG, recursive search. Default is %(default)s.')
     args_paser.add_argument('-j', '--join-str', type=str, default=' ',
                             help='join string of file name. Default is %(default)s.')
+    args_paser.add_argument('-rmd', '--remove-dir', action='store_true', default=False,
+                            help='FLAG, remove empty dir after move files. Default is %(default)s.')
     args = args_paser.parse_args(sys_args)
     # process args
     args.type = args.type.split(',')
     args.input = clean_path(args.input)
-    show_args(args, ['type', 'input', 'name', 'recursive', 'join_str'])
+    show_args(args, ['type', 'input', 'name', 'recursive', 'join_str', 'remove_dir'])
     
     paths = get_paths_with_extension(args.input, args.type,
                                      args.recursive, args.name)
-    print(f'files to remove: {len(paths)}')
-    # manual confirm
-    inputs = input('Are you sure to delete these files? (y/n) ')
-    if inputs.lower() != 'y':
-        print('cancel delete files')
-        return []
+    print(f'files to move: {len(paths)} (include root dir to skip)')
     # move files
     root = str(args.input)
+    empty_dirs = []
     for path in tqdm(paths):
         try:
             path = str(clean_path(path))
-            dist = path.replace(root, '').replace(os.path.sep, ' ')
+            dist = path.replace(root, '').replace(os.path.sep, args.join_str)
             dist = os.path.join(root, dist)
-            shutil.move(path, dist)
+            if root != dist:
+                shutil.move(path, dist)
+                if os.path.dirname(path) not in empty_dirs:
+                    empty_dirs.append(os.path.dirname(path))
         except Exception as e:
             put_err(f'Error: {e}. Can not move {path}, skip')
+    # remove empty dir
+    if args.remove_dir:
+        for empty_dir in empty_dirs:
+            try:
+                os.rmdir(empty_dir)
+            except Exception as e:
+                put_err(f'Error: {e}. Can not remove empty dir {empty_dir}, skip')
+    # return paths
     return paths
     
     
