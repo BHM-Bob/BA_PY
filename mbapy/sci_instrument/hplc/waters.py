22cfad1ec7e77e7c69415d15e0c0162ec4fc4d6e
BHM-Bob <2262029386@qq.com>
2024-05-22 09:56:39 +0800

    fix-code: sci_instrument.hplc.waters.WatersData: fix to _base.Hplcdata

--- mbapy/sci_instrument/hplc/waters.py
+++ mbapy/sci_instrument/hplc/waters.py
@@ -9,34 +9,24 @@ import pandas as pd
 if __name__ == '__main__':
     from mbapy.base import put_err, parameter_checker, check_parameters_path
     from mbapy.file import decode_bits_to_str, write_sheets, opts_file
-    from mbapy.sci_instrument.hplc._base import HPLC_Data
+    from mbapy.sci_instrument.hplc._base import HplcData
 else:
     from ...base import put_err, parameter_checker, check_parameters_path
     from ...file import decode_bits_to_str, write_sheets, opts_file
-    from ._base import HPLC_Data
+    from ._base import HplcData
     
 
-class WatersData(HPLC_Data):
-    @parameter_checker(data_file_path=lambda path: path is None or (check_parameters_path(path) and (path.endswith('.arw') or path.endswith('.xlsx'))))
+class WatersData(HplcData):
+    @parameter_checker(data_file_path=lambda path: path is None or (check_parameters_path(path) and path.endswith('.arw')))
     def __init__(self, data_file_path: str = None) -> None:
-        super().__init__(str(Path(data_file_path).resolve()))
+        super().__init__(data_file_path)
         self.raw_data = self.load_raw_data_file()
         self.processed_data = self.process_raw_data() if self.raw_data else None
         self.tag = self.make_tag() if self.processed_data else None
-        self.process_raw_data_path: str = None
+        self.processed_data_path: str = None
         self.X_HEADER = 'Time'
         self.Y_HEADER = 'Absorbance'
         self.TICKS_IN_MINUTE = 60 # how many ticks in one minute
-        
-    def load_raw_data_file(self, raw_data_bytes: bytes = None):
-        if raw_data_bytes is None and self.data_file_path:
-            raw_data_bytes = opts_file(self.data_file_path, 'rb')
-        else:
-            return None # no waring, return None
-        return decode_bits_to_str(raw_data_bytes)
-    
-    def load_raw_data_from_bytes(self, raw_data_bytes: bytes):
-        return self.load_raw_data_file(raw_data_bytes)
     
     @parameter_checker(path=lambda path: path is None or check_parameters_path(path))
     def load_processed_data_file(self, path: str = None, data_bytes: bytes = None):
@@ -57,12 +47,16 @@ class WatersData(HPLC_Data):
         return tag
     
     def process_raw_data(self, *args, **kwargs):
-        lines = self.raw_data.splitlines()
-        info_df = pd.DataFrame([lines[1].split('\t')], columns = lines[0].split('\t'))
-        data_df = pd.DataFrame([line.split('\t') for line in lines[2:]],
-                               columns = ['Time', 'Absorbance']).astype({'Time': float, 'Absorbance': float})
-        self.data_df, self.info_df = data_df, info_df
-        return info_df, data_df
+        try:
+            lines = self.raw_data.splitlines()
+            info_df = pd.DataFrame([lines[1].split('\t')], columns = lines[0].split('\t'))
+            data_df = pd.DataFrame([line.split('\t') for line in lines[2:]],
+                                columns = [self.X_HEADER, self.Y_HEADER]).astype({self.X_HEADER: float, self.Y_HEADER: float})
+            self.data_df, self.info_df = data_df, info_df
+            self.SUCCEED_LOADED = True
+            return info_df, data_df
+        except:
+            return put_err('Failed to process raw data, return None')
     
     def get_abs_data(self, *args, **kwargs):
         return self.processed_data[1] if self.processed_data else self.process_raw_data()[1]
@@ -71,15 +65,9 @@ class WatersData(HPLC_Data):
         if not self.processed_data:
             self.process_raw_data()
         if path is None:
-            path = self.process_raw_data_path = Path(self.data_file_path).with_suffix('.xlsx')
+            path = self.processed_data_path = Path(self.data_file_path).with_suffix('.xlsx')
         write_sheets(path, {'Info': self.info_df, 'Data': self.data_df})
         return path
-    
-    def get_processed_data(self, *args, **kwargs):
-        return self.processed_data or self.process_raw_data()
-    
-    def get_tag(self, *args, **kwargs):
-        return self.tag or self.make_tag()
 
 
 __all__ = [
@@ -88,6 +76,7 @@ __all__ = [
 
 
 if __name__ == '__main__':
+    WatersData()
     data = WatersData('data_tmp/scripts/hplc/ORI_DATA5184.arw')
     data.save_processed_data()
     print(data.get_tag())
