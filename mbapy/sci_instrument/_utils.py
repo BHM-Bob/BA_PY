3b6cb38f0a4704584ec3eccef4800435517d1144
BHM-Bob <2262029386@qq.com>
2024-06-01 19:55:33 +0800

    addon: mbapy\sci_instrument\_utils.py: add process_num_label_col_marker

--- mbapy/sci_instrument/_utils.py
+++ mbapy/sci_instrument/_utils.py
@@ -1,11 +1,19 @@
+'''
+Date: 2024-05-25 08:43:43
+LastEditors: BHM-Bob 2262029386@qq.com
+LastEditTime: 2024-06-01 19:55:07
+Description: 
+'''
+
+from typing import List
 
 if __name__ == '__main__':
-    from mbapy.plot import get_palette
+    from mbapy.plot import PLT_MARKERS, get_palette
 else:
-    from ..plot import get_palette
+    from ..plot import PLT_MARKERS, get_palette
     
     
-def process_label_col(labels: str, file_col_mode = 'hls'):
+def process_label_col(labels: str, file_col_mode: str = 'hls'):
     labels = '' if labels is None else labels
     file_labels, colors = [], get_palette(len(labels.split(';')), mode = file_col_mode)
     for idx, i in enumerate(labels.split(';')):
@@ -15,7 +23,7 @@ def process_label_col(labels: str, file_col_mode = 'hls'):
             file_labels.append([label, color])
     return file_labels
 
-def process_num_label_col(labels: str, peak_col_mode = 'hls'):
+def process_num_label_col(labels: str, peak_col_mode: str = 'hls'):
     labels = '' if labels is None else labels
     peak_labels, cols = {}, get_palette(len(labels.split(';')), mode = peak_col_mode)
     for i, label in enumerate(labels.split(';')):
@@ -26,4 +34,20 @@ def process_num_label_col(labels: str, peak_col_mode = 'hls'):
             elif len(items) == 3:
                 t, label, color = items
             peak_labels[float(t)] = [label, color]
+    return peak_labels
+
+def process_num_label_col_marker(labels: str, peak_col_mode: str = 'hls',
+                                 markers: List[str] = PLT_MARKERS):
+    labels = '' if labels is None else labels
+    peak_labels, cols = {}, get_palette(len(labels.split(';')), mode = peak_col_mode)
+    for i, label in enumerate(labels.split(';')):
+        if label:
+            items = label.split(',')
+            if len(items) == 2:
+                (t, label), color, maker = items, cols[i], markers[i]
+            elif len(items) == 3:
+                (t, label, color), maker = items, markers[i]
+            elif len(items) == 4:
+                t, label, color, maker = items
+            peak_labels[float(t)] = [label, color, maker]
     return peak_labels
