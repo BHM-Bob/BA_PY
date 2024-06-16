318ff3e43e3674da8e939e61654c7fdd0cccddfa
BHM-Bob <2262029386@qq.com>
2024-06-16 10:59:10 +0800

    v0.8.4

--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2023-07-29 09:56:37
  * @LastEditors: BHM-Bob 2262029386@qq.com
- * @LastEditTime: 2024-06-14 16:57:32
+ * @LastEditTime: 2024-06-16 10:54:11
  * @Description: 
 -->
 <!-- mkdocs serve -->
@@ -112,6 +112,7 @@ Some helpful command line scripts.
 16. [video](scripts/video.md)  
 
 # Release History
+- [0.8.4](release_notes/0.8.4.md)
 - [0.8.3](release_notes/0.8.3.md)  
 - [0.8.2](release_notes/0.8.2.md)  
 - [0.8.1](release_notes/0.8.1.md)  
--- mbapy/__version__.py
+++ mbapy/__version__.py
@@ -2,15 +2,15 @@
 Author: BHM-Bob 2262029386@qq.com
 Date: 2022-11-01 22:16:49
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-06-13 10:36:23
+LastEditTime: 2024-06-16 10:53:21
 Description: 
 '''
 
 __title__ = "mbapy"
 __description__ = "MyBA in Python"
 __url__ = "https://github.com/BHM-Bob/BA_PY"
-__version__ = "0.8.3"
-__build__ = "20240610_0"
+__version__ = "0.8.4"
+__build__ = "20240616_0"
 __author__ = "BHM-Bob G"
 __author_email__ = "bhmfly@foxmail.com"
 __license__ = "MIT"
