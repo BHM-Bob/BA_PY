82202911a0b8b066a10a6e0c2415b58641936929
BHM-Bob <2262029386@qq.com>
2024-06-30 17:20:09 +0800

    minor-addon: mbapy\sci_instrument\mass\__init__.py: feat(mbapy): introduce SciexMZMine for MZMine data support

--- mbapy/sci_instrument/mass/__init__.py
+++ mbapy/sci_instrument/mass/__init__.py
@@ -1,19 +1,19 @@
 '''
 Date: 2024-05-20 17:05:00
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-05-30 17:23:11
+LastEditTime: 2024-06-30 17:19:17
 Description: 
 '''
 
 from ._base import MassData
 from ._utils import plot_mass, process_peak_labels
-from .SCIEX import SciexOriData, SciexPeakListData
-
+from .SCIEX import SciexMZMine, SciexOriData, SciexPeakListData
 
 __all__ = [
     'MassData',
     'plot_mass',
     'process_peak_labels',
+    'SciexMZMine',
     'SciexOriData',
     'SciexPeakListData',
 ]
