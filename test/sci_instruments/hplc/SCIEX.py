ef00a56d489a8473793847eccd6b955aadb78b34
BHM-Bob <2262029386@qq.com>
2024-07-03 21:48:38 +0800

    feat(tests): add unit tests for Sciex data processing in HPLC module
    Extend the unittest suite for the HPLC SCIEX data processing functionalities.
    This includes the addition of tests for loading raw data files and processed data
    files, ensuring the correctness of data loading and processing mechanisms.

--- test/sci_instruments/hplc/SCIEX.py
+++ test/sci_instruments/hplc/SCIEX.py
@@ -1,12 +1,30 @@
 '''
 Date: 2024-05-21 22:16:15
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-05-21 22:31:31
+LastEditTime: 2024-07-03 21:46:07
 Description:
 '''
 import unittest
+
 import pandas as pd
+
 from mbapy.sci_instrument.hplc import SciexData, SciexTicData
+
+
+class TestSciexData(unittest.TestCase):
+
+    def setUp(self):
+        self.data_file_path = 'data_tmp/scripts/hplc/SCIEX.txt'
+        self.processed_data_path = 'data_tmp/scripts/hplc/SCIEX.xlsx'
+
+    def test_load_raw_data_file(self):
+        data = SciexTicData(self.data_file_path)
+        raw_data = data.load_raw_data_file()
+
+    def test_load_processed_data_file(self):
+        data = SciexData(self.data_file_path)
+        data.save_processed_data()
+        data.load_processed_data_file(self.processed_data_path)
 
 class TestSciexTicData(unittest.TestCase):
 
