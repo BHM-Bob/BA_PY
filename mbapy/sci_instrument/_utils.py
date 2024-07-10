bdca320bdbfc464c458616ab11dbf1c14a3413e8
BHM-Bob <2262029386@qq.com>
2024-07-10 11:20:24 +0800

    docs(mbapy\sci_instrument\_utils.py): add docstrings for label processing functions
    Add detailed docstrings to clarify the usage and functionality of the
    label processing utility functions including `process_label_col`,
    `process_num_label_col`, and `process_num_label_col_marker`. This
    improves code readability and helps users understand how to effectively
    use these functions for label information processing.

--- mbapy/sci_instrument/_utils.py
+++ mbapy/sci_instrument/_utils.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-05-25 08:43:43
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-06-01 19:55:07
+LastEditTime: 2024-07-10 11:19:39
 Description: 
 '''
 
@@ -14,6 +14,20 @@ else:
     
     
 def process_label_col(labels: str, file_col_mode: str = 'hls'):
+    """
+    This function is used to process label information, extract and format labels\n
+    LABEL1,COLOR1;LABEL2,COLOR2;...
+
+    Parameters:
+        - labels (str): Label string, separated by semicolons
+        - file_col_mode (str): File column mode, default is 'hls'
+
+    Return:
+        - file_labels (list): List containing the processed label information
+        
+    Example:
+    >>> process_label_col('file1,red;file2,blue', 'hls')
+    """
     labels = '' if labels is None else labels
     file_labels, colors = [], get_palette(len(labels.split(';')), mode = file_col_mode)
     for idx, i in enumerate(labels.split(';')):
@@ -24,6 +38,20 @@ def process_label_col(labels: str, file_col_mode: str = 'hls'):
     return file_labels
 
 def process_num_label_col(labels: str, peak_col_mode: str = 'hls'):
+    """
+    This function is used to process label information, extract and format labels\n
+    NUMBER1,LABEL1,COLOR1;NUMBER2,LABEL2,COLOR2;...
+
+    Parameters:
+        - labels (str): Label string, separated by semicolons
+        - peak_col_mode (str): Peak column mode, default is 'hls'
+
+    Return:
+        - peak_labels (dict): Dictionary containing the processed label information
+        
+    Example:
+    >>> process_num_label_col('10,peak1,red;20,peak2,blue', 'hls')
+    """
     labels = '' if labels is None else labels
     peak_labels, cols = {}, get_palette(len(labels.split(';')), mode = peak_col_mode)
     for i, label in enumerate(labels.split(';')):
@@ -38,6 +66,21 @@ def process_num_label_col(labels: str, peak_col_mode: str = 'hls'):
 
 def process_num_label_col_marker(labels: str, peak_col_mode: str = 'hls',
                                  markers: List[str] = PLT_MARKERS):
+    """
+    This function is used to process label information, extract and format labels\n
+    NUMBER1,LABEL1,COLOR1,MARKER1;NUMBER2,LABEL2,COLOR2,MARKER2;...
+
+    Parameters:
+        - labels (str): Label string, separated by semicolons
+        - peak_col_mode (str): Peak column mode, default is 'hls'
+        - markers (List[str]): List of marker types, default is PLT_MARKERS
+
+    Return:
+        - peak_labels (dict): Dictionary containing the processed label information
+        
+    Example:
+    >>> process_num_label_col_marker('10,peak1,red,o;20,peak2,blue,v', 'hls', ['o', 'v', '^'])
+    """
     labels = '' if labels is None else labels
     peak_labels, cols = {}, get_palette(len(labels.split(';')), mode = peak_col_mode)
     for i, label in enumerate(labels.split(';')):
