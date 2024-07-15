c29005f3757d34cac6fe0382bbab1ad9dc1db0f9
BHM-Bob <2262029386@qq.com>
2024-07-15 10:27:20 +0800

    feat(mbapy-scripts): add just_name option for original file name usage
    Add a new flag '--just-name' to the move_file script which allows users to
    use the original name of the files instead of the sub-path when generating the
    output path. This provides more flexibility in how files are renamed and moved.

--- mbapy/scripts/move_file.py
+++ mbapy/scripts/move_file.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-06-15 12:08:22
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-07-11 07:50:41
+LastEditTime: 2024-07-15 10:25:08
 Description: 
 '''
 
@@ -35,13 +35,15 @@ def main(sys_args: List[str] = None):
                             help='sub-string of name of files to remove. Default is %(default)s')
     args_paser.add_argument('-r', '--recursive', action='store_true', default=False,
                             help='FLAG, recursive search. Default is %(default)s.')
+    args_paser.add_argument('--just-name', action='store_true', default=False,
+                            help='FLAG, Use original name of files instead of sub-path. Default is %(default)s.')
     args = args_paser.parse_args(sys_args)
     
     # process args
     args.type = args.type.split(',')
     args.input = clean_path(args.input)
     args.output = clean_path(args.output)
-    show_args(args, ['input', 'output', 'type', 'name', 'recursive'])
+    show_args(args, ['input', 'output', 'type', 'name', 'recursive', 'just_name'])
     
     # short cut if only a single file to move
     if os.path.isfile(args.input) and not os.path.isdir(args.output):
@@ -55,17 +57,24 @@ def main(sys_args: List[str] = None):
     # move
     for path in tqdm(paths):
         try:
-            sub_path = path[len(str(args.input)):]
-            if sub_path.startswith(os.path.sep):
-                sub_path = sub_path[len(os.path.sep):]
-            output_path = os.path.join(args.output, sub_path)
+            # make output path
+            if args.just_name:
+                output_path = os.path.join(args.output, os.path.basename(path))
+            else:
+                sub_path = path[len(str(args.input)):]
+                if sub_path.startswith(os.path.sep):
+                    sub_path = sub_path[len(os.path.sep):]
+                output_path = os.path.join(args.output, sub_path)
+            # check output path
             output_dir = os.path.dirname(output_path)
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
+            # check system permission
             if platform.system().lower() == 'windows':
                 os.system(f'attrib -r "{path}"')
             elif platform.system().lower() == 'linux':
                 os.system(f'chmod 666 "{path}"')
+            # move file
             shutil.move(path, output_path)
         except Exception as e:
             put_err(f'can not move {path} with error {e}, skip')
