b4d105a4762e813ee3f8d24f019dda2902e141d3
BHM-Bob <2262029386@qq.com>
2024-07-17 09:30:35 +0800

    fix(mbapy/test/dl_t/optim): update import and iteration syntax for clarity
    Refactor the import statement _str2scheduleF to str2scheduleF

--- test/dl_t/optim.py
+++ test/dl_t/optim.py
@@ -1,7 +1,7 @@
 '''
 Date: 2023-05-26 19:30:28
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2023-05-26 19:48:52
+LastEditTime: 2024-07-17 08:47:42
 FilePath: /BA_PY/mbapy/test/dl_t/optim.py
 Description: 
 '''
@@ -10,12 +10,12 @@ import torch.optim as optim
 
 import matplotlib.pyplot as plt
 
-from mbapy.dl_torch.optim import _str2scheduleF, LrScheduler
+from mbapy.dl_torch.optim import str2scheduleF, LrScheduler
 
 lr = 0.01
 optimizer = optim.SGD(nn.Linear(1, 1).parameters(), lr)
 
-for method in _str2scheduleF.keys():
+for method in str2scheduleF.keys():
     scheduler = LrScheduler(optimizer, lr, 0, method=method)
     lrs = [scheduler.step(epoch) for epoch in range(scheduler.sum_epoch)]
     plt.plot(list(range(scheduler.sum_epoch)), lrs)
