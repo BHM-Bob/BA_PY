ed96368657022b31e1bbc6c30eb3fd815bb11ee4
BHM-Bob <2262029386@qq.com>
2025-01-26 12:12:20 +0800

    fix-bug: mbapy\scripts\_script_utils_.py: excute_command: check callable first to avoid `TypeError: issubclass() arg 1 must be a class`
    - Update the command execution logic to support both callable functions and Command instances
    - This change allows for more flexible command handling and potential bug fixes in command execution

--- mbapy/scripts/_script_utils_.py
+++ mbapy/scripts/_script_utils_.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-01-12 16:06:35
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2025-01-26 11:38:52
+LastEditTime: 2025-01-26 12:02:36
 FilePath: \BA_PY\mbapy\scripts\_script_utils_.py
 Description: 
 '''
@@ -80,7 +80,9 @@ def excute_command(args_paser: argparse.ArgumentParser, sys_args: List[str],
     
     if args.sub_command in _str2func:
         try:
-            if issubclass(_str2func[args.sub_command], Command):
+            if callable(_str2func[args.sub_command]):
+                _str2func[args.sub_command](args)
+            elif isinstance(_str2func[args.sub_command], Command):
                 _str2func[args.sub_command](args).excute()
             else:
                 _str2func[args.sub_command](args)
