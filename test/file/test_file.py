c19b9a050e50cd60ee4ec954cd00255f7527fa54
BHM-Bob <2262029386@qq.com>
2025-02-10 19:47:12 +0800

    test(file): add unit tests for get_paths_with_extension function
    - Implement comprehensive tests for get_paths_with_extension function
    - Cover various scenarios including recursive search, file extensions, name substrings, and sorting options
    - Add test cases for natural sorting and unknown sort options
    - Ensure proper setup and teardown of test environment

--- test/file/test_file.py
+++ test/file/test_file.py
@@ -1,16 +1,15 @@
 '''
 Date: 2024-11-10 21:55:48
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2024-11-10 22:02:02
+LastEditTime: 2025-02-10 19:44:06
 Description: 
 '''
-import unittest
 import os
-import json
 import tempfile
-import pandas as pd
+import unittest
 
-from mbapy.file import opts_file
+from mbapy.file import get_paths_with_extension, opts_file
+
 
 class TestOptsFile(unittest.TestCase):
     
@@ -45,6 +44,64 @@ class TestOptsFile(unittest.TestCase):
         opts_file(self.pkl_file_path, mode='wb', way='pkl', data=data)
         read_data = opts_file(self.pkl_file_path, mode='rb', way='pkl')
         self.assertEqual(read_data, data)
+
+class TestGetPaths(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.test_dir = tempfile.TemporaryDirectory()
+        self.test_folder = os.path.join(self.test_dir.name, 'test_folder')
+        os.makedirs(self.test_folder)
+        
+        self.file1 = os.path.join(self.test_folder, 'file1.txt')
+        self.file2 = os.path.join(self.test_folder, 'file2.jpg')
+        self.subdir = os.path.join(self.test_folder, 'subdir')
+        os.makedirs(self.subdir)
+        self.file10 = os.path.join(self.subdir, 'file10.txt')
+        self.file3 = os.path.join(self.subdir, 'file3.txt')
+        
+        with open(self.file1, 'w') as f:
+            f.write('This is file1.txt')
+        with open(self.file2, 'w') as f:
+            f.write('This is file2.jpg')
+        with open(self.file3, 'w') as f:
+            f.write('This is file3.txt')
+        with open(self.file10, 'w') as f:
+            f.write('This is file10.txt')
+
+    def tearDown(self):
+        self.test_dir.cleanup()
+
+    def test_get_paths_with_extension_WithExtension(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt'], recursive=False)
+        self.assertEqual(result, [self.file1])
+
+    def test_get_paths_with_extension_NoExtension(self):
+        result = get_paths_with_extension(self.test_folder, [], recursive=False)
+        self.assertEqual(result, [self.file1, self.file2])
+
+    def test_get_paths_with_extension_Recursive(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt', '.jpg'], recursive=True)
+        self.assertEqual(result, [self.file1, self.file2, self.file10, self.file3])
+
+    def test_get_paths_with_extension_NameSubstring(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt'], name_substr='file1')
+        self.assertEqual(result, [self.file1, self.file10])
+
+    def test_get_paths_with_extension_SearchNameInDir(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt'], search_name_in_dir=True, name_substr='sub')
+        self.assertEqual(result, [self.subdir])
+
+    def test_get_paths_with_extension_Sort(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt', '.jpg'], sort=True)
+        self.assertEqual(result, [self.file1, self.file2, self.file10, self.file3])
+
+    def test_get_paths_with_extension_NaturalSort(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt'], sort='natsort')
+        self.assertEqual(result, [self.file1, self.file3, self.file10])
+
+    def test_get_paths_with_extension_UnknownSortOption(self):
+        result = get_paths_with_extension(self.test_folder, ['.txt'], sort='unknown')
+        self.assertEqual(result, [self.file1, self.file10, self.file3])
 
 if __name__ == '__main__':
     unittest.main()
