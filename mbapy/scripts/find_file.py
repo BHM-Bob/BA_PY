7f370c92e31c8ae52a9ef320db2d40f8d115a38f
BHM-Bob <2262029386@qq.com>
2025-08-09 09:44:29 +0800

    minor-addon: mbapy\scripts\find_file.py: use c version of get_paths_with_extension, add sum option to enable size summary

--- mbapy/scripts/find_file.py
+++ mbapy/scripts/find_file.py
@@ -1,7 +1,7 @@
 '''
 Date: 2024-07-16 13:43:25
 LastEditors: BHM-Bob 2262029386@qq.com
-LastEditTime: 2025-02-15 11:47:55
+LastEditTime: 2025-08-09 09:18:53
 Description: 
 '''
 
@@ -29,6 +29,8 @@ def main(sys_args: List[str] = None):
                             help='output list txt file path or dir path. Default is %(default)s.')
     args_paser.add_argument('-r', '--recursive', action='store_true', default=False,
                             help='FLAG, recursive search. Default is %(default)s.')
+    args_paser.add_argument('-s', '--sum', action='store_true', default=False,
+                            help='FLAG, sum size of files. Default is %(default)s.')
     args = args_paser.parse_args(sys_args)
     
     # process IO path
@@ -42,12 +44,19 @@ def main(sys_args: List[str] = None):
     show_args(args, ['type', 'name', 'input', 'output', 'recursive'])
     
     paths = get_paths_with_extension(args.input, args.type,
-                                     args.recursive, args.name)
+                                     args.recursive, args.name, c_version=True)
     _print(f'files finded: {len(paths)} in dir: {args.input}', f_handle)
-    # delete files
-    for path in paths:
-        info_str = ', '.join([path[len(str(args.input)):], format_file_size(os.path.getsize(path))])
-        _print(info_str, f_handle)
+    # show info
+    if args.sum:
+        total_size = 0
+        for path in paths:
+            total_size += os.path.getsize(path)
+        _print(f'total files size: {format_file_size(total_size)}', f_handle)
+    else:
+        for path in paths:
+            info_str = ', '.join([path[len(str(args.input)):], format_file_size(os.path.getsize(path))])
+            _print(info_str, f_handle)
+    _print(f'files finded: {len(paths)} in dir: {args.input}', f_handle) # print again in bottom
     return paths
 
 
